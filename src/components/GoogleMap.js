a761f21bf4721557e756a6fd66d1051ed0124af1
chrome <lutfiandwica@gmail.com>
2016-12-07 14:36:05 +0700

    fixed set marker position on nextProps

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -2,24 +2,43 @@ import React from 'react';
 
 export default class GoogleMap extends React.Component {
   shouldComponentUpdate() {
+    // This component must render one time only
+    // and never render it again
     return false;
   }
 
   componentWillReceiveProps(nextProps) {
-    this.map.panTo({ lat: nextProps.lat, lng: nextProps.lng });
+    const nextLatLng = { lat: nextProps.lat, lng: nextProps.lng };
+
+    // Center map to next latitude and longtitude
+    this.map.panTo(nextLatLng);
+
+    // Set map position on current clicked area
+    this.marker.setPosition(nextLatLng);
   }
 
   componentDidMount() {
+    const myLatLng = { lat: this.props.lat, lng: this.props.lng };
+
+    // Create new Map and center it with default coordinates
     this.map = new google.maps.Map(this.refs.map, {
-      center: { lat: this.props.lat, lng: this.props.lng },
+      center: myLatLng,
       zoom: 10
     });
 
-    this.map.addListener('click', function(e) {
-      const lat = e.latLng.lat();
-      const lng = e.latLng.lng();
+    // Create new Marker
+    this.marker = new google.maps.Marker({
+      map: this.map
+    });
+
+    // Event if Map is clicked
+    this.map.addListener('click', (event) => {
+      const lat = event.latLng.lat();
+      const lng = event.latLng.lng();
+
+      // Fetch weather by current coordinates
       this.props.actions.fetchWeatherByCoord(lat, lng);
-    }.bind(this));
+    });
 
   }
 
