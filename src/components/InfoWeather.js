c2edd6e93c99d1b0a02bae089f9da4e873b6486c
chrome <lutfiandwica@gmail.com>
2016-12-08 13:51:03 +0700

    fixed if city name has 3 words or more

--- src/components/InfoWeather.js
+++ src/components/InfoWeather.js
@@ -5,12 +5,24 @@ export default function InfoWeather(props) {
 
   const isEmpty = Object.keys(weather).length === 0;
 
-  const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
   const kelvinToCelcius = (temperature) => {
     return Math.floor( Number(temperature) - 273.15 );
+  }
+
+  const sliceToTwoWords = (str) => {
+    const arrayOfStr = str.split(' ');
+    const onlyTwoWords = arrayOfStr.splice(0, 2);
+    const newStr = onlyTwoWords.join(' ');
+
+    if (arrayOfStr.length <= 2) {
+      return str;
+    }
+
+    return newStr + '...';
   }
 
   if(isEmpty) {
@@ -31,17 +43,17 @@ export default function InfoWeather(props) {
           className="infoWeather_icon" />
         <div className="infoWeather_text">
           <span className="infoWeather_temperature">
-            {kelvinToCelcius(weather.main.temp)}
+            {kelvinToCelcius( weather.main.temp )}
             &deg;
             {'C'}
           </span>
           <div className="infoWeather_weatherInfo">
-            {capitalizeFirstLetter(weather.weather[0].description)}
+            {capitalizeFirstLetter( weather.weather[0].description )}
           </div>
         </div>
       </div>
       <div className="infoWeather_cityName">
-          {weather.name}
+        {sliceToTwoWords( weather.name )}
       </div>
 
     </div>
